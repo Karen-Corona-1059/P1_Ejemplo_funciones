7520c133a3630f98ae8ac949eb862d9396513a9f
Karen Corona <164534032+Karen-Corona-1059@users.noreply.github.com>
2025-02-07 16:47:44 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,20 @@
+ //funcion que no devuelve valor
+
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Karen Corona ');
+}
+
+void main(){
+  //Karen Corona Mat:22308051281059
+  //llamando a la funcion saludar
+  
+  print('Karen Corona Mat:22308051281059 \n');
+  print('Llamando a la funcion saludar');
+  saludar();
+  print('\nLlamando a la funcion escribeNombre');
+  escribeNombre();
+}
