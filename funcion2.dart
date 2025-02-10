2c09b0e00a44295b60e4455d18166287fbcf205f
Karen Corona <164534032+Karen-Corona-1059@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+//fnciones creadas por el usuario
+void suma(int a, int b){
+  print('La suma de $a + $b es: ${a+b}');
+}
+void resta(int a, int b){
+  print('La resta de $a - $b es: ${a-b}');
+} 
+
+//funcion main
+void main(){
+  print('Karen Corona Mat:22308051281059 \n');
+  print('Llamando a la funcion suma y resta');
+  suma(10, 5);
+  resta(10, 5);
+}
