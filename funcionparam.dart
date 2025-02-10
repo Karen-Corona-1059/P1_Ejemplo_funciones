9188452b08fbcd890837645df8bcdac7274a9d75
Karen Corona <164534032+Karen-Corona-1059@users.noreply.github.com>
2025-02-10 18:47:07 +0000

    funcion parametros

--- funcionparam.dart
+++ funcionparam.dart
@@ -8,6 +8,9 @@ double calcularAreaRectangulo(double longitud, double ancho) {
 
 void main() {
   print('Karen Corona Mat:22308051281059 \n');
+
+  print("Llamando a la función saludar");
   print(saludar("Karen", "Corona")); 
+  print("\nLlamando a la función calcularAreaRectangulo");
   print(calcularAreaRectangulo(5.0, 10.0)); 
 }
